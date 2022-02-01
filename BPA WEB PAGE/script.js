8a56690976a236d9eaaca38e0c6ec21788350ae6
Avinash Gupta <93194744+AvinashTokyo@users.noreply.github.com>
2022-01-31 20:21:24 -0800

    Add files via upload

---
+++ BPA WEB PAGE/script.js
@@ -0,0 +1,10 @@
+// just for practice
+function secclick(){
+    document.getElementById("sec-head").innerHTML = "Wow! u r doing great job";
+}
+// just for practice
+function sechover(){
+    document.getElementById("sec-head").innerHTML = "Wow! u r doing great job";
+}
+
+//Gallery Script
